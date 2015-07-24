0f815a10c58ba20cb272a22f5e3ec92acc96e715
Juan Ángel Celdrán <jaceldran@gmail.com>
2015-07-24 13:30:51 +0200

    Revert "ejecución local, versión y despliegue en heroku"
    This reverts commit a9ab6af4d41246cc78700e09d1a22af86a57e9a2.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,28 +1,23 @@
-// se añade gestión de modelos
-
-var models = require('../models/models.js');
-
 // GET /quizes/question
 
 exports.question = function (req, res) 
 {
-	models.Quiz.findAll().success(function (quiz) {
-		res.render('quizes/question', {pregunta: quiz[0].pregunta})
-	});
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 // GET /quizes/question
 
 exports.answer = function (req, res) 
 {
-	models.Quiz.findAll().success(function (quiz) {
-		if (req.query.respuesta === quiz[0].respuesta)
-		{
-			res.render('quizes/answer', {respuesta: 'Correcto'});
-		}
-		else
-		{
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
-		}		
-	});
+
+console.log(res.query)
+
+	if (req.query.respuesta === 'Roma')
+	{
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	}
+	else
+	{
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
 };
