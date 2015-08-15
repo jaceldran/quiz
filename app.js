1f551b56ac3343c199d3601d5d0549c5cce2f036
Juan Ángel Celdrán <jaceldran@gmail.com>
2015-08-15 08:18:07 +0200

    ajustes versión final - 2

--- app.js
+++ app.js
@@ -45,41 +45,28 @@ app.use( function(req, res, next) {
 	
 	if (req.session.user) {
 
-		// limite para auto-logout en milisegundos
-
-		var limit = 2 * 60  * 1000; // 2 minutos
 		var username = req.session.user.username;
-
-		// registrar cuando se inicia la sesión
-
-		if (req.session.start===undefined) {
-			console.log('=>[APP-SESSION-START ' + username + ']');
-			req.session.start = new Date().getTime();
-		}
-	
-		// calcular duración de la sesión y comparar con el límite	
-	
-		var duration = new Date().getTime() - req.session.start;
-		if (duration > limit) {
+		var limit = 1*60*1000;
+		var now = new Date().getTime();	
 		
-			console.log('=>[APP-AUTO-LOGOUT ' + username + ']');
-			
-			// reiniciar control de duración
-			
-			delete req.session.start;
-
-			// [[ esto da error de "session undefined" (????)
-			//var sessionController = require('./controllers/session_controller');
-			//sessionController.destroy(); // ]]
-
-			// como lo anterior no funciona, 
-			// copio el código del método session_controller.destroy()
-			// excepto el res.redirect que da error de "header sent"
-			
-			delete req.session.user;
+		// calcular límite de caducidad (en ms) a partir del 
+		// último acceso (req.session.last) y, si es anterior
+		// al momento actual (now), resetear session
+		
+		if (!(req.session.last===undefined)) {		
+			limit += req.session.last;
+			console.log('=>[APP-SESSION-START ' + username + ']');
+			if ( now > limit) {			
+				console.log('=>[APP-AUTO-LOGOUT ' + username + ']');
+				delete req.session.last;
+				delete req.session.user;
+			}			
 		}
+		
+		// registrar hora de último acceso
+		
+		req.session.last = now;		
 	}
-
 
 	// next lo que sea
 	// antes justo bajo la línea:  res.locals.session = req.session;
